85fe620104c983de8c4b4f33b4bbe5b0a3744575
kidag34378 <kidag34378@toracw.com>
2020-07-21 16:08:10 +0200

    api

--- js/gameListHandler.js
+++ js/gameListHandler.js
@@ -1,18 +1,17 @@
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
+const apiUrl = "https://www.roblox.com/Thumbs/Asset.ashx?format=png&width=420&height=230&assetId=";
 
 var getGameList = function () {
-    return httpGet("https://pastebin.com/raw/2BbD5XBK");
+    fetch("https://pastebin.com/raw/2BbD5XBK").then(function (response) {
+        return response;
+    })
 }
-
-// https://www.roblox.com/Thumbs/Asset.ashx?format=png&width=420&height=230&assetId=0
 
 var getGameIcons = function () {
     let gameList = getGameList();
+    for (i = 0; i < gameList[games].length; i++) {
+        let game = gameList[games][i];
+        let gameIconUrl = apiUrl + game.id;
+        fetch(gameIconUrl);
+    }
     console.log(gameList);
 }
