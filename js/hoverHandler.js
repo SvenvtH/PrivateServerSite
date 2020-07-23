b2fa8abed3598e4f58ca451214e2e52e5896ba8e
kidag34378 <kidag34378@toracw.com>
2020-07-23 02:29:02 +0200

    Feature added, Subscription passes

--- js/hoverHandler.js
+++ js/hoverHandler.js
@@ -1,4 +1,5 @@
 const gameList = ["Ro-Ghoul", "Arsenal", "Jailbreak", "MM2", "AFS", "LT2", "ABD", "SO"];
+const shoppyList = ["GoldPass", "RubyPass", "UraniumPass", "VibraniumPass"];
 
 var showText = function(gameId) {
     let text = gameList[gameId - 1];
@@ -9,4 +10,15 @@ var showText = function(gameId) {
 var hideText = function(gameId) {
     document.getElementById(`img-${gameId}`).style = "filter: none;"
     document.getElementById(`text-${gameId}`).innerHTML = "";
+}
+
+var showShoppyText = function(passId) {
+    let text = shoppyList[passId - 1];
+    document.getElementById(`imgShoppy-${passId}`).style = "filter: blur(3px);"
+    document.getElementById(`textShoppy-${passId}`).innerHTML = text;
+}
+
+var hideShoppyText = function(gameId) {
+    document.getElementById(`imgShoppy-${gameId}`).style = "filter: none;"
+    document.getElementById(`textShoppy-${gameId}`).innerHTML = "";
 }
